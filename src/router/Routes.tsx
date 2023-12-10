af911d1fe84a9b2c5b63c60717c9910e25f7e61e
lekhanhlinh-star <82929088+lekhanhlinh-star@users.noreply.github.com>
2023-12-10 17:39:29 +0700

    linh an

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -2,22 +2,23 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Test from "../components/test";
 import Login_page from "../pages/LoginPage";
 import {AdminPage} from "../pages/admin/AdminPage";
-
-const Routes=()=>{
 import Home_page from "../pages/home";
-const Routes = () => {
-    const routesForPublic = [{
-            path:"/",element:<Test></Test>
-        },
-        {
-            path: "/login",element: <Login_page/>
-        }
-        ,
-        {
-            path: "/admin",element: <AdminPage></AdminPage>
-        }
-    ]
-    const router=createBrowserRouter(routesForPublic )
-    return <RouterProvider router={router}/>
-}
+
+
+ const Routes = () => {
+     const routesForPublic = [
+         {
+             path: "/", element: <Home_page/>
+         },
+         {
+             path: "/login", element: <Login_page/>
+         }
+         ,
+         {
+             path: "/admin", element: <AdminPage></AdminPage>
+         }
+     ]
+     const router = createBrowserRouter(routesForPublic)
+     return <RouterProvider router={router}/>
+ }
 export default Routes
